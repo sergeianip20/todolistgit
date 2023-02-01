80f597644a32a8fd48dec2482a49614d671b5165
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-02-01 13:12:37 +0400

    Update AddTitlespan.tsx
    Use

--- src/AddTitlespan.tsx
+++ src/AddTitlespan.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useState, useCallback} from 'react';
 import TextField from '@mui/material/TextField';
 
 type EditableSpanPropsType = {
@@ -10,17 +10,17 @@ export const  EditableSpan = React.memo ((props: EditableSpanPropsType) => {
     let [editMode, setEditMode] = useState(false);
     let [title, setTitle] = useState(props.value);
 
-    const activateEditMode = () => {
+    const activateEditMode = useCallback(() => {
         setEditMode(true);
         setTitle(props.value);
-    }
-    const activateViewMode = () => {
+    },[])
+    const activateViewMode = useCallback( () => {
         setEditMode(false);
         props.onChange(title);
-    }
-    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    },[props.onChange])
+    const changeTitle = useCallback( (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
-    }
+    },[])
 
     return editMode
         ? <TextField value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
